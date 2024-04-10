40da008480fe158f136f525d8f36ec3812cb6ae6
_eyewave <89079979+eye-wave@users.noreply.github.com>
2024-04-10 21:21:50 +0200

    fix: swap b and g colors

--- src/color.rs
+++ src/color.rs
@@ -3,7 +3,7 @@ use plotters::style::RGBAColor;
 
 pub fn parse_hex_color(input: &str) -> Option<RGBAColor> {
     if let Ok(color) = HexColor::parse(input) {
-        return Some(RGBAColor(color.r, color.b, color.g, color.a as f64));
+        return Some(RGBAColor(color.r, color.g, color.b, color.a as f64));
     }
 
     None
