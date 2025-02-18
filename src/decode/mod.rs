cf62f10a3da37699d889d2418d8c6693b92e0d4e
_eyewave <89079979+eye-wave@users.noreply.github.com>
2025-02-18 15:41:23 +0100

    rewrite opus decoding

--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -5,8 +5,8 @@ use std::path::Path;
 use symphonia::{create_probe, decode_audio, get_cover_art};
 
 pub mod midi;
-mod opus;
 mod symphonia;
+mod opus;
 
 #[cfg_attr(debug_assertions, derive(Debug))]
 pub enum VisualData {
